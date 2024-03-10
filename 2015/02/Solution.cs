8f037fdd211a86efccecdf5ec1d6a08e380a8160
Terrence Zhang <96602253+ZhangTerrence@users.noreply.github.com>
2024-03-10 19:41:06 -0400

    Solved 2015/02

---
+++ 2015/02/Solution.cs
@@ -0,0 +1,60 @@
+using AdventOfCode.Lib;
+
+namespace AdventOfCode._2015._02;
+
+[Solution("I Was Told There Would Be No Math", 2015, 02)]
+public class Solution(string inputPath) : SolutionBase
+{
+    private string InputPath { get; } = inputPath;
+    private List<string>? Input { get; set; }
+
+    public override void PrintSolutions()
+    {
+        Input = File.ReadLines(InputPath).ToList();
+
+        var wrappingPaper = SolvePartOne();
+        Console.WriteLine($"\ufe42 Part 1 => {wrappingPaper}");
+
+        var ribbon = SolvePartTwo();
+        Console.WriteLine($"\ufe42 Part 2 => {ribbon}");
+    }
+
+    private int SolvePartOne()
+    {
+        var input = Input ?? throw new NullReferenceException("Input is null.");
+
+        return input.Select(line =>
+                line.Split("x").Select(int.Parse).OrderBy(e => e).ToList())
+            .Aggregate(0,
+                (acc, dimensions) =>
+                {
+                    int x = dimensions[0], y = dimensions[1], z = dimensions[2];
+
+                    var wrappingPaper = 2 * x * y +
+                                        2 * y * z +
+                                        2 * x * z +
+                                        x * y;
+
+                    acc += wrappingPaper;
+
+                    return acc;
+                });
+    }
+
+    private int SolvePartTwo()
+    {
+        var input = Input ?? throw new NullReferenceException("Input is null.");
+
+        return input.Select(line => line.Split("x").Select(int.Parse).OrderBy(e => e).ToList()).Aggregate(0,
+            (acc, dimensions) =>
+            {
+                int x = dimensions[0], y = dimensions[1], z = dimensions[2];
+
+                var ribbon = 2 * x + 2 * y + x * y * z;
+
+                acc += ribbon;
+
+                return acc;
+            });
+    }
+}
