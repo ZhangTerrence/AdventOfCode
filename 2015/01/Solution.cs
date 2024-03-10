84c74a23eb971e39db86d523bd9bb75055feb8d5
Terrence Zhang <96602253+ZhangTerrence@users.noreply.github.com>
2024-03-10 17:15:30 -0400

    Solved 2015/01

---
+++ 2015/01/Solution.cs
@@ -0,0 +1,49 @@
+using AdventOfCode.Lib;
+
+namespace AdventOfCode._2015._01;
+
+[Solution("Not Quite Lisp", 2015, 01)]
+public class Solution(string inputPath) : SolutionBase
+{
+    private string InputPath { get; } = inputPath;
+    private string? Input { get; set; }
+
+    public override void PrintSolutions()
+    {
+        Input = File.ReadAllText(InputPath);
+
+        var floor = SolvePartOne();
+        Console.WriteLine($"\ufe42 Part 1 => {floor}");
+
+        var pos = SolvePartTwo();
+        Console.WriteLine($"\ufe42 Part 2 => {pos}");
+    }
+
+    private int SolvePartOne()
+    {
+        var input = Input ?? throw new NullReferenceException("Input is null.");
+        var floor = 0;
+
+        foreach (var e in input)
+            if (e == '(') floor++;
+            else floor--;
+
+        return floor;
+    }
+
+    private int SolvePartTwo()
+    {
+        var input = Input ?? throw new NullReferenceException("Input is null.");
+        var floor = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (floor == -1) return i;
+
+            if (input[i] == '(') floor++;
+            else floor--;
+        }
+
+        return -1;
+    }
+}
